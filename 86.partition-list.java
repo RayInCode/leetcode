6d036ab04dd6009f92a91c4a0a0fce4ef4df8cef
Lei <yl739678679@gmail.com>
2023-06-08 14:56:18 -0500

    modify 86

--- 86.partition-list.java
+++ 86.partition-list.java
@@ -19,26 +19,28 @@ class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode ldummy = new ListNode();
         ListNode rdummy = new ListNode();
-        ListNode lnode = ldummy;
-        ListNode rnode = rdummy;
+        ListNode lp = ldummy, rp = rdummy;
+        ListNode p = head;
 
-        while(head != null) {
-            if(head.val < x) {
-                lnode.next = head;
-                lnode = lnode.next;
+        while(p != null) {
+            if(p.val < x) {
+                lp.next = p;
+                lp = lp.next;
             }else {
-                rnode.next = head;
-                rnode = rnode.next;
+                rp.next = p;
+                rp = rp.next;
             }
-            head = head.next;
+            p = p.next;
         }
         
         /*  !!! without this statement, there may be a cycle 
             in the result when the last node in the orginial 
             list is less than x, because the last rnode would
             point to the last node in the left list*/ 
-        rnode.next = null;   
-        lnode.next = rdummy.next;
+        rp.next = null;
+        
+        // another advantage of dummy node when we need to return head node of a list
+        lp.next = rdummy.next;
         return ldummy.next;
     }
 }
